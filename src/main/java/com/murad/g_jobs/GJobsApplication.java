4c6d6921d37233375ef39a61a8f9be771cb71115
MOURAD ELGOURARI <muradelgourarion15@gmail.com>
2025-11-08 11:09:32 +0100

    Initial commit

---
+++ src/main/java/com/murad/g_jobs/GJobsApplication.java
@@ -0,0 +1,13 @@
+package com.murad.g_jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murad/g_jobs/GJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murad.g_jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
